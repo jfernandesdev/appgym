e4404cd96f7e5eee363684dc6ac923daaa076438
Jeferson Fernandes <jfernandes.dev@gmail.com>
2024-09-01 21:14:11 -0300

    CHORE: importing expo-google-fonts/roboto fonts and styling the status bar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024'}}>
+      <StatusBar 
+        backgroundColor='transparent'
+        barStyle='light-content'
+        translucent
+      />
+      { fontsLoaded ? <Text style={{ fontSize: 40, fontFamily: 'Roboto_700Bold', color: '#FFF'}}>Home</Text> : <View/>}
     </View>
   );
 }
