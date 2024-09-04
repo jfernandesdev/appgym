94e49909689ef8c70d02f8c0a2ed463cb4046b8c
Jeferson Fernandes <jfernandes.dev@gmail.com>
2024-09-03 22:29:15 -0300

    FEAT: button component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,11 @@
 import { VStack, Image, Center, Text, Heading } from "@gluestack-ui/themed";
 
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
 import BackgroundSignIn from "@assets/background.png";
 import Logo from "@assets/logo.svg";
-import { Input } from "@components/Input";
+
 
 export function SignIn() {
   return (
@@ -32,6 +35,18 @@ export function SignIn() {
             autoCapitalize="none" 
           />
           <Input placeholder="Senha" secureTextEntry />
+
+          <Button title="Acessar" />
+        </Center>
+
+        <Center flex={1} justifyContent="flex-end" mt="$4">
+          <Text 
+            color="$gray100" 
+            fontSize="$sm"
+            mb="$3"
+            fontFamily="$body"
+          >Ainda não tem acesso?</Text>
+          <Button title="Criar conta" variant="outline" />
         </Center>
       </VStack>
     </VStack>
