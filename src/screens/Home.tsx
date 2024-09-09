0f323ca990ebd2bcf62450f9008c9b8b978d6071
Jeferson Fernandes <jfernandes.dev@gmail.com>
2024-09-08 21:56:37 -0300

    FEAT: muscle group component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,29 @@
-import { VStack } from "@gluestack-ui/themed";
+import { useState } from "react";
+import { HStack, VStack } from "@gluestack-ui/themed";
 
 import { HomeHeader } from "@components/HomeHeader";
+import { MuscleGroup } from "@components/MuscleGroup";
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState("costa");
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <HStack gap="$3">
+        <MuscleGroup 
+          label="Costa" 
+          isActive={groupSelected === "costa"}
+          onPress={() => setGroupSelected("costa")}
+        />
+
+        <MuscleGroup
+          label="Ombro"
+          isActive={groupSelected === "ombro"}
+          onPress={() => setGroupSelected("ombro")}
+        />
+      </HStack>
     </VStack>
   )
 }
